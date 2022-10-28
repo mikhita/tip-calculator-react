4a143935e6d0b774838d1bcd85c3a0e3befc4ec9
Mikheil Ispiryan <106303545+mikhita@users.noreply.github.com>
2022-10-28 20:46:13 +0400

    Update MainDiv.js

--- tip-calculator/src/components/MainDiv.js
+++ tip-calculator/src/components/MainDiv.js
@@ -1,47 +1,56 @@
-import React, {useState , useEffect} from 'react'
+import React, {useState } from 'react'
 import "./Component.css"
 import styled from 'styled-components'
 import SelectInputs from './SelectInputs.js'
 import TipAmount from './TipAmount.js'
-import { useForm , useWatch } from "react-hook-form"
+import { useForm } from "react-hook-form"
 import { yupResolver } from "@hookform/resolvers/yup";
 import schema from '../schema.js';
 import Inputs from './Inputs.js'
 function MainDiv() {
-    const {register, handleSubmit, control , formState: { errors, dirtyFields, isDirty } } = useForm({
+    const {register,  formState: { errors } } = useForm({
         criteriaMode: "all",
         mode: "onChange",
         resolver: yupResolver(schema)
       });
-      const onSubmit = data => console.log(data);
+      
       const [isChoosen, setIsChoosen] = useState();
       const [isBill, setIsBill] = useState();
       const [numPeople, setNumPeople] = useState();
-      const [tipAmount, setTipAmount] = useState(0);
-    const [totalAmount, setTotalAmount] = useState(0);
+      const handleChange = (e)=>setNumPeople(e.target.value);
+      const handleChange1 = (e)=>setIsChoosen(e.target.value);
+      const handleChange2 = (e)=>setIsBill(e.target.value);
+      
 
-    const tipReset = (isBill*isChoosen/100)/numPeople
-    const totalReset = (parseInt(isBill)+isBill*isChoosen/100)/numPeople
-    console.log(typeof(isBill))
+
+    let tipReset = parseInt((isBill*isChoosen/100)/numPeople)
+    tipReset = tipReset || 0;
+    let totalReset = parseInt((parseInt(isBill)+isBill*isChoosen/100)/numPeople)
+    totalReset = totalReset || 0;
+    console.log(tipReset)
       const array = [5,10,15,25,50];
   return (
     <MainDivs>
         <WhiteSide>
         <form>
-            <Inputs type="number" id="billInput" register={register} value={isBill} onChange={(e)=>setIsBill(e.target.value)} label="billInput"
+            <Inputs type="number" id="billInput"  register={register} value={isBill} onChange={handleChange2} 
+            label="billInput"
             placeholder="0" text="Bill" error={errors.billInput && errors.billInput.message }/>
             <SecondP>Select Tip %</SecondP>
             <PercentDiv>
                 <SelectInputs isChoosen={isChoosen} setIsChoosen={setIsChoosen} array={array}/>
-                <Inputs value={isChoosen} noMargin onChange={(e)=>setIsChoosen(e.target.value)} type="number" id="percent" register={register} label="percent" error={errors.percent && errors.percent.message }/>
+                <Inputs value={isChoosen} noMargin  onChange={handleChange1} type="number" id="percent" 
+                register={register} 
+                label="percent" error={errors.percent && errors.percent.message }/>
             </PercentDiv>
-            <Inputs classname="numogp" value={numPeople} onChange={(e)=>setNumPeople(e.target.value)} type="number" id="numOfPeople" register={register} label="numOfPeople"
+            <Inputs classname="numogp" value={numPeople}  onChange={handleChange} type="number" id="numOfPeople" 
+            register={register} label="numOfPeople"
             placeholder="0" text="Number Of People" error={errors.numOfPeople && errors.numOfPeople.message } />
         </form>
         </WhiteSide>
         <GreenSide>
            <TipAmount  tip={tipReset} total={totalReset}/>   
-           <ResetB>
+           <ResetB onClick={() => window.location.reload(false)}>
             Reset
            </ResetB>
         </GreenSide>
@@ -58,8 +67,6 @@ const PercentDiv = styled.div`
     display: grid;
     grid-template-columns: repeat(2, 1fr);
     gap: 10px;
-    max-width: 327px;
-   
     @media (min-width: 950px) {
       grid-template-columns: repeat(3, 1fr);
       max-width: -webkit-fill-available;
@@ -84,11 +91,14 @@ const MainDivs = styled.div`
     justify-content: space-around;
     border-radius: 25px;
     margin: 0%;
-    
-
   }
 `;
 const WhiteSide = styled.div`
+width: 90%;
+@media (min-width: 950px) {
+    max-width: 40%;
+
+}
     
 `;
 const SecondP = styled.p`
@@ -107,10 +117,11 @@ const GreenSide = styled.div`
     border-radius: 15px; 
     padding: 19px 24px 24px 24px;
     margin-top: 32px;
-    max-width: -webkit-fill-available;
+    width: 90%;
     @media (min-width: 950px) {
     height: 360px;
     justify-content: space-around;
+    width: 40%;
 }
 
 `;
